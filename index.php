ad61d7cf3d4b0dc0850b1a396b60c157af57560a
Donatella7 <campaniellodonatella@gmail.com>
2022-04-21 16:09:20 +0200

    header

--- index.php
+++ index.php
@@ -1,5 +1,32 @@
 <?php
 
+    $header_menu = [
+        [
+            "title" => "Overview",
+            "link" => "https://policies.google.com/?hl=en-US",
+        ],
+
+        [
+            "title" => "Privacy policy",
+            "link" => "https://policies.google.com/privacy?hl=en-US",
+        ],
+
+        [
+            "title" => "Terms of service",
+            "link" => "https://policies.google.com/terms?hl=en-US",
+        ],
+
+        [
+            "title" => "Technologies",
+            "link" => "https://policies.google.com/technologies?hl=en-US",
+        ],
+
+        [
+            "title" => "Faq",
+            "link" => "http://localhost/php-google-faq/",
+        ],
+    ];
+
     $google_faqs = [
         
         [
@@ -45,17 +72,32 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <title>Google</title>
-</head>
+    <link rel="stylesheet" href="css/style.css">
+    <title> Google</title>
+</head> 
 <body>
     <header>
         <section class="container-fluid">
-            <div class="row">
+            <div class="row pt-2 px-2">
+                <div class="col-12 d-flex align-items-center">
+                    <img class="logo-size me-1" src="img/google-hd-logo.png" alt="logo google">
+                    <h2 class="text-capitalize text-secondary">Privacy & terms</h2>
+                </div>
                 <div class="col-12">
-                    <h1>Google</h1>
+                    <ul class="list-unstyled d-flex">
+
+                        <?php foreach($header_menu as $menu_page) { ?>
+                            <li class="mx-2">
+                                <a class="text-decoration-none" href="<?php echo $menu_page["link"] ?>">
+                                    <h5> <?php echo $menu_page["title"] ?> </h5>
+                                </a>
+                            </li>
+                        <?php } ?>
+                    </ul>
                 </div>
             </div>
         </section>
+        <hr>
     </header>
 
     <main>
